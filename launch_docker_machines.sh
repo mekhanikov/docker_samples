d0fe3df9e76a798b9f745b4787d21d28c3f4da8c
Evgeny Mekhanikov <emekhanikov@gmail.com>
2016-05-16 22:48:48 +0600

    swarm experements

--- launch_docker_machines.sh
+++ launch_docker_machines.sh
@@ -32,7 +32,7 @@ scp -i /home/dev/docker_samples/lxc-agent2/.vagrant/machines/default/lxc/private
 ssh -oStrictHostKeyChecking=no -i /home/dev/docker_samples/lxc-ca/.vagrant/machines/default/lxc/private_key vagrant@192.161.59.101 "mkdir -p /home/vagrant/.certs"
 scp -i /home/dev/docker_samples/lxc-ca/.vagrant/machines/default/lxc/private_key ./ca.pem vagrant@192.161.59.101:/home/vagrant/.certs/ca.pem
 scp -i /home/dev/docker_samples/lxc-ca/.vagrant/machines/default/lxc/private_key ./ca-priv-key.pem vagrant@192.161.59.101:/home/vagrant/.certs/ca-priv-key.pem
-scp -i /home/dev/docker_samples/lxc-ca/.vagrant/machines/default/lxc/private_key ./ca-cert.pem vagrant@192.161.59.101:/home/vagrant/.certs/ca-cert.pem
+#scp -i /home/dev/docker_samples/lxc-ca/.vagrant/machines/default/lxc/private_key ./ca-cert.pem vagrant@192.161.59.101:/home/vagrant/.certs/ca-cert.pem
 scp -i /home/dev/docker_samples/lxc-ca/.vagrant/machines/default/lxc/private_key ./manager-priv-key.pem vagrant@192.161.59.101:/home/vagrant/.certs/manager-priv-key.pem
 scp -i /home/dev/docker_samples/lxc-ca/.vagrant/machines/default/lxc/private_key ./manager-cert.pem vagrant@192.161.59.101:/home/vagrant/.certs/manager-cert.pem
 scp -i /home/dev/docker_samples/lxc-ca/.vagrant/machines/default/lxc/private_key ./agent1-priv-key.pem vagrant@192.161.59.101:/home/vagrant/.certs/agent1-priv-key.pem
